8e358f6ec0161213cf6fccd667ce5cb34f72cd6f
ZdwCode <276448724@qq>
2020-07-11 19:47:03 +0800

    2.custom

--- app/src/main/java/com/example/luck/MainActivity.kt
+++ app/src/main/java/com/example/luck/MainActivity.kt
@@ -2,11 +2,19 @@ package com.example.luck
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.constraintlayout.widget.ConstraintLayout
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var luck=Luck(this)
+
+        var constraintLayout: ConstraintLayout=findViewById(R.id.rootlayout);
+
+        constraintLayout.addView(luck);
+
     }
 }
